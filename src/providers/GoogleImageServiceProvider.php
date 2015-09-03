967056a7d136e5712780f3893cacf00d3e27c2a3
David Maes <david.s.maes@gmail.com>
2015-09-04 00:08:17 +0200

    Initialize module

---
+++ src/providers/GoogleImageServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Bendani\PhpCommon\GoogleImageService\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+
+class GoogleImageServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+//        $this->package('bendani/php-common/login-service', 'common-login');
+        $this->package('bendani/php-common/google-image-service');
+
+//        $view = View::make('common-login::login-form');
+
+//        View::addNamespace('bendani/php-common/google-image-service', __DIR__.'/../views');
+    }
+
+
+}
