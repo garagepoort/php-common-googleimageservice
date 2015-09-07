0b9739b5faf0436ff1e0e06ba6e8a7007019afed
David Maes <david.s.maes@gmail.com>
2015-09-07 22:24:06 +0200

    use properties for cx and key

--- src/providers/GoogleImageServiceProvider.php
+++ src/providers/GoogleImageServiceProvider.php
@@ -26,8 +26,8 @@ class GoogleImageServiceProvider extends ServiceProvider
     public function boot()
     {
 //        $this->package('bendani/php-common/login-service', 'common-login');
-        $this->package('bendani/php-common/google-image-service', null, __DIR__.'/../');
-//        $this->package('bendani/php-common/google-image-service');
+//        $this->package('bendani/php-common/google-image-service', null, __DIR__.'/../');
+        $this->package('bendani/php-common/google-image-service');
 
 //        $view = View::make('common-login::login-form');
 
