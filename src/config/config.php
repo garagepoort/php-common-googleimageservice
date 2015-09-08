db85660fba51b705a368b7f2054c2554e2c58c1d
David Maes <david.s.maes@gmail.com>
2015-09-08 19:38:52 +0200

    changed config

--- src/config/config.php
+++ src/config/config.php
@@ -1,5 +1,5 @@
 <?php
 return array(
-    'googleImageCx' => '003701006117915782863%3Agrbhoeigwrg',
-    'googleImageKey' => 'AIzaSyBPkSNgxg9ZjRgYnS0Fj6fZ9xw-Xn5l_pw',
+    'googleImageCx' => '',
+    'googleImageKey' => '',
 );
