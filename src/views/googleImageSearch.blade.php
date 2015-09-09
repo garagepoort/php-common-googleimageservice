6ffd1b46a84ff309a2caaf03530dbe0e3f22cfd8
David Maes <david.s.maes@gmail.com>
2015-09-09 22:18:25 +0200

    change execute search function

--- src/views/googleImageSearch.blade.php
+++ src/views/googleImageSearch.blade.php
@@ -3,75 +3,79 @@
     <table class="google-image-search-table" hidden>
     </table>
 </div>
-<div id="branding"  style="float: left;"></div><br />
+<div id="branding" style="float: left;"></div><br/>
 <input id={{ $imageUrlInput }} name={{ $imageUrlInput }} hidden>
 
 <script type="text/javascript">
-    var contentDivId;
-    var googleImageSearchTable;
-    var loader;
-    var imageUrlInput;
 
-    function executeGoogleSearch(searchString) {
-        contentDivId = {{ $contentDivId }};
-        imageUrlInput = {{ $imageUrlInput }};
+    $(function () {
+        var contentDivId;
+        var googleImageSearchTable;
+        var loader;
+        var imageUrlInput;
 
-        loader = $('#' + contentDivId + ' > :nth-child(1)');
-        googleImageSearchTable = $('#' + contentDivId + ' > :nth-child(2)');
+        function executeGoogleSearch(searchString) {
+            contentDivId = {{ $contentDivId }};
+            imageUrlInput = {{ $imageUrlInput }};
 
-        googleImageSearchTable.hide();
-        loader.show();
+            loader = $('#' + contentDivId + ' > :nth-child(1)');
+            googleImageSearchTable = $('#' + contentDivId + ' > :nth-child(2)');
 
-        //imageSearch.execute(searchString);
+            googleImageSearchTable.hide();
+            loader.show();
 
-        var cx = {{ Config::get("GIS::googleImageCx") }};
-        var key = {{ Config::get("GIS::googleImageKey") }};
-        $.get('https://www.googleapis.com/customsearch/v1?cx=' + cx + '&key=' + key + '&searchType=image&imgSize=medium&alt=json&q=' + searchString, function (data) {
-            results = data.items;
+            //imageSearch.execute(searchString);
 
-            if (results && results.length > 0) {
+            var cx = {{ Config::get("GIS::googleImageCx") }};
+            var key = {{ Config::get("GIS::googleImageKey") }};
+            $.get('https://www.googleapis.com/customsearch/v1?cx=' + cx + '&key=' + key + '&searchType=image&imgSize=medium&alt=json&q=' + searchString, function (data) {
+                results = data.items;
+
+                if (results && results.length > 0) {
 
 
-                googleImageSearchTable.empty();
+                    googleImageSearchTable.empty();
 
-                var tableBody = $('<tbody></tbody>');
-                var tableRow = $('<tr></tr>');
-                googleImageSearchTable.append(tableBody);
-                tableBody.append(tableRow);
-                for (var i = 0; i < results.length; i++) {
-                    if (i == 5) {
-                        var tableRow = $('<tr></tr>');
-                        tableBody.append(tableRow);
+                    var tableBody = $('<tbody></tbody>');
+                    var tableRow = $('<tr></tr>');
+                    googleImageSearchTable.append(tableBody);
+                    tableBody.append(tableRow);
+                    for (var i = 0; i < results.length; i++) {
+                        if (i == 5) {
+                            var tableRow = $('<tr></tr>');
+                            tableBody.append(tableRow);
+                        }
+                        // For each result write it's title and image to the screen
+                        var result = results[i];
+                        var tableData = $("<td></td>");
+
+                        var newImg = $('<img width="150px">');
+                        newImg.attr('src', result.image.thumbnailLink);
+                        newImg.attr('imageUrl', result.link);
+                        if (result.url === $('#' + imageUrlInput).val()) {
+                            newImg.addClass("google-selected-image");
+                            newImg.addClass(contentDivId);
+                        }
+
+                        tableData.append(newImg);
+
+                        tableRow.append(tableData);
+
+                        newImg.on('click', function () {
+                            $('.' + contentDivId).removeClass("google-selected-image");
+                            $('.' + contentDivId).removeClass(contentDivId);
+                            $(this).addClass("google-selected-image");
+                            $(this).addClass(contentDivId);
+                            $('#' + imageUrlInput).val($(this).attr('imageUrl'));
+                        });
                     }
-                    // For each result write it's title and image to the screen
-                    var result = results[i];
-                    var tableData = $("<td></td>");
-
-                    var newImg = $('<img width="150px">');
-                    newImg.attr('src', result.image.thumbnailLink);
-                    newImg.attr('imageUrl', result.link);
-                    if (result.url === $('#' + imageUrlInput).val()) {
-                        newImg.addClass("google-selected-image");
-                        newImg.addClass(contentDivId);
-                    }
-
-                    tableData.append(newImg);
-
-                    tableRow.append(tableData);
-
-                    newImg.on('click', function () {
-                        $('.' + contentDivId).removeClass("google-selected-image");
-                        $('.' + contentDivId).removeClass(contentDivId);
-                        $(this).addClass("google-selected-image");
-                        $(this).addClass(contentDivId);
-                        $('#' + imageUrlInput).val($(this).attr('imageUrl'));
-                    });
+                    loader.hide();
+                    googleImageSearchTable.show();
                 }
-                loader.hide();
-                googleImageSearchTable.show();
-            }
-        }).fail(function() {
-            alert( "error" );
-        });
-    }
+            }).fail(function () {
+                alert("error");
+            });
+        }
+    });
+
 </script>
